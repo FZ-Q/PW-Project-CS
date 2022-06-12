524714059c8c5232d195e0f586febc6cdb8c2b57
lingaauliaa <linggarishar@gmail.com>
2022-06-12 17:25:47 +0700

    register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="style/style.css">
-  <title>Document</title>
+  <title>Register</title>
 </head>
 
 <body class="background-login">
@@ -18,18 +18,17 @@
       <div class="row justify-content-center">
         <div class="col-md-6">
           <div class="p-4" style="background-color: #f9f8f6;">
-            @if ($errors->any())
-            <div class="alert alert-danger">
-              <ul>
-                @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-              </ul>
-            </div>
-            @endif
-            <h1 class="text-center">Login</h1>
-            <form action="{{url('auth')}}" method="POST">
-              @csrf
+            <h1 class="text-center">Register</h1>
+            <form action="{{url('register-action')}}" method="POST" enctype="multipart/form-data">
+                @csrf
+            <div class="mb-3">
+                  <label for="formFile" class="form-label">Gambar</label>
+                  <input class="form-control" type="file" id="formFile" name="image" required>
+                </div>
+              <div class="mb-3">
+                <label for="INama" class="form-label">Nama</label>
+                <input type="text" class="form-control" placeholder="Nama" name="name" id="INama" required>
+              </div>
               <div class="mb-3">
                 <label for="iEmail" class="form-label">Email address</label>
                 <input type="email" class="form-control" placeholder="Email" name="email" id="iEmail" required>
@@ -39,16 +38,16 @@
                 <input type="password" class="form-control" placeholder="Password" name="password" id="iPass" required>
               </div>
               <div class="mb-3">
-                <button name="submit" class="btn btn-primary">Login</button>
+                <button name="submit" class="btn btn-primary">Register</button>
               </div>
-              <p class="login-register-text">Anda belum punya akun? <a href="{{url('register')}}">Register</a></p>
+              <p class="login-register-text">Anda sudah punya akun? <a href="login.php">Login </a></p>
             </form>
           </div>
         </div>
       </div>
     </div>
   </main>
-
+  
 </body>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
